e01a8e4551ef7110ba5fab2c4483c055af57e658
JPCM95 <juanpablo.cubero@gmail.com>
2015-04-03 00:22:10 -0600

    XMLReader fix#2
    Updated with better documentation

--- XMLReader.java
+++ XMLReader.java
@@ -24,6 +24,12 @@ import org.jdom2.JDOMException;
 
 import org.jdom2.input.SAXBuilder;
 
+/**
+ * 
+ * @author Juan Pablo Cubero
+ * This class initializes the method to read an XML file.
+ */
+
 public class XMLReader {
 	public static void main(String[] args) {		
 		readXML();
@@ -34,20 +40,33 @@ public class XMLReader {
 		SAXBuilder builder = new SAXBuilder();
 		try {
 			
-			// Parses the file supplied into a JDOM document
-			
+			/**
+                         * Parses the file supplied into a JDOM document
+			*/
 			Document readDoc = builder.build(new File("C:\\Users\\Juan Pablo Cubero\\Desktop\\Datos I Nereo\\Proyectos\\I Proyecto (SpiderBot y Engine)\\SpiderBot\\URLs.xml"));
 						
-			// Gets the text found between the name tags
-			
+			/**
+                         * Gets the text found between the name tags
+			*/
 			System.out.println("URL1: " + readDoc.getRootElement().getChildText("URL1"));
 			System.out.println("URL2: " + readDoc.getRootElement().getChildText("URL2"));
 			System.out.println("URL3: " + readDoc.getRootElement().getChildText("URL3"));
 			System.out.println("URL4: " + readDoc.getRootElement().getChildText("URL4"));
 			System.out.println("\n");
-
-			Element root = readDoc.getRootElement();
+                        
+                        /**
+                         * Creates a list with the first URLs to analyze
+                        */
+                        
+                        String lista[] = {readDoc.getRootElement().getChildText("URL1"),readDoc.getRootElement().getChildText("URL2"),readDoc.getRootElement().getChildText("URL3"),readDoc.getRootElement().getChildText("URL4")};
+                        
+                        for (int x=0; x<lista.length;x++)
+                        System.out.println(lista[x]);
 			
+                        Element root = readDoc.getRootElement();
+			
+                        System.out.println("Esto es de la lista con los URL");
+                        System.out.println("\n");
 		
 			
 		} 
@@ -60,17 +79,20 @@ public class XMLReader {
 			e.printStackTrace();
 		} 
 		
-//------------------------------HERE IS WHERE CONFIGURATIONS IS------------------------------------------------------------
+                
+//------------------------------HERE IS WHERE THE CONFIGURATIONS IS------------------------------------------------------------
 		
                 SAXBuilder builder2 = new SAXBuilder();
 		try {
 			
-			// Parses the file supplied into a JDOM document
-			
+			/**
+                         * Parses the file supplied into a JDOM document
+			*/
 			Document readDoc = builder.build(new File("C:\\Users\\Juan Pablo Cubero\\Desktop\\Datos I Nereo\\Proyectos\\I Proyecto (SpiderBot y Engine)\\SpiderBot\\Configurations.xml"));
 						
-			// Gets the text found between the name tags
-			
+			/**
+                         * Gets the text found between the name tags
+			*/
 			System.out.println("maxthreads: " + readDoc.getRootElement().getChildText("maxthreads"));
 			System.out.println("recursivity: " + readDoc.getRootElement().getChildText("recursivity"));
 			System.out.println("reindex: " + readDoc.getRootElement().getChildText("reindex"));
@@ -89,6 +111,10 @@ public class XMLReader {
 			e.printStackTrace();
 		}
         
+                
+                
+                
         }
 	
 }
+
