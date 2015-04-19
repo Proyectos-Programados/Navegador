9fa2890f40e300698da424ed516df27d5a1a51c0
JPCM95 <juanpablo.cubero@gmail.com>
2015-04-19 18:01:06 +0200

    Final update for XMLWriter.java

--- XMLWriter.java
+++ XMLWriter.java
@@ -1,4 +1,3 @@
-
 package XMLWriter;
 
 import java.io.File; // Class that will represent a system file name
@@ -29,6 +28,10 @@ public class XMLWriter {
 /**
  * Introduces the name of the parts of the new XML file, and add them to it.
  */
+ 
+/**-----------------------------------------------------------------------------
+ * This is where the URLs XML is written
+ */
 private static void writeXML(){
     try{
     Document doc = new Document();
@@ -37,7 +40,7 @@ private static void writeXML(){
     doc.setRootElement(theRoot);
 
     Element url1 = new Element("URL1");
-    url1.addContent(new Text("http://es.wikipedia.org"));
+    url1.addContent(new Text("https://es.wikipedia.org/wiki"));
         
     Element url2 = new Element("URL2");
     url2.addContent(new Text("http://amazon.com"));
@@ -55,9 +58,42 @@ private static void writeXML(){
     
     XMLOutputter xmlOutput = new XMLOutputter(Format.getPrettyFormat());
     
-    xmlOutput.output(doc, new FileOutputStream(new File("C:\\Users\\Juan Pablo Cubero\\Desktop\\Datos I Nereo\\Proyectos\\I Proyecto (SpiderBot y Engine)\\SpiderBot\\URLs.xml")));
+    xmlOutput.output(doc, new FileOutputStream(new File("C:\\SpiderBot\\URLs.xml")));
     
     System.out.println("Wrote to file");
+    }
+    catch (Exception ex){
+    ex.printStackTrace();
+        
+    }
+    /**-----------------------------------------------------------------------------
+     * This is where the configurations XML is written
+     */
+    try{
+    Document doc = new Document();
+
+    Element theRoot = new Element ("spider");
+    doc.setRootElement(theRoot);
+
+    Element maxthreads = new Element("maxthreads");
+    maxthreads.addContent(new Text("20"));
+        
+    Element recursivity = new Element("recursivity");
+    recursivity.addContent(new Text("10"));
+        
+    Element reindex = new Element("reindex");
+    reindex.addContent(new Text("56000"));
+    
+    
+    theRoot.addContent(maxthreads);
+    theRoot.addContent(recursivity);
+    theRoot.addContent(reindex);
+    
+    XMLOutputter xmlOutput = new XMLOutputter(Format.getPrettyFormat());
+    
+    xmlOutput.output(doc, new FileOutputStream(new File("C:\\SpiderBot\\Configurations.xml")));
+    
+    System.out.println("Wrote to file...Again");
     }
     catch (Exception ex){
     ex.printStackTrace();
